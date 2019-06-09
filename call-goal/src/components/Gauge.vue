e8235b9cd3cce5d0bfd73bda116b7205720d22e5
Breno Rios <rbrenorios@gmail.com>
2019-06-09 15:13:43 -0300

    Dashboard screen complete

--- call-goal/src/components/Gauge.vue
+++ call-goal/src/components/Gauge.vue
@@ -1,11 +1,14 @@
 <template>
-  <apexchart
-    class="chart fadeInUp"
-    width="700"
-    type="radialBar"
-    :options="chartOptions"
-    :series="series"
-  ></apexchart>
+  <div class="gauge">
+    <h2 :class="[isDone? titleCongrat: titleNormal]">{{title}}</h2>
+    <apexchart
+      :class="[isDone? chartCongrat: chartNormal]"
+      width="600"
+      type="radialBar"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
 </template>
 
 <style lang="less">
@@ -13,14 +16,25 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-  // justify-content: center;
   width: 80%;
   height: 80%;
 }
-
 .chart {
-  // background-color: red;
   margin: 0px 0px 0px 0px;
+}
+
+.title {
+  margin: 30px 0px 0px 0px;
+}
+
+.congratChart {
+  animation-name: "bounce";
+  animation-duration: 0.7s;
+}
+.congratTitle {
+  animation-name: "bounce";
+  animation-delay: 0.1s;
+  animation-duration: 0.8s;
 }
 </style>
 
@@ -32,20 +46,23 @@ export default {
   components: {
     apexchart: VueApexCharts
   },
-  updated() {
-    console.log("Changed");
-    this.series[0] = this.done;
+  props: {
+    callGoal: Number,
+    callDone: Number
   },
-
-  mounted() {
-    console.log("GAUGEGOAL: " + this.goal);
+  beforeMount() {
+    this.title = "0 left to go";
   },
   data() {
     return {
       type: "radialBar",
-      // done: 0,
-      goal: 0,
-      series: [30, 20],
+      series: [0],
+      title: "",
+      titleNormal: "title fadeInUp",
+      titleCongrat: "title congratTitle",
+      chartNormal: "chart fadeInUp",
+      chartCongrat: "chart congratChart",
+      isDone: false,
       chartOptions: {
         plotOptions: {
           radialBar: {
@@ -64,7 +81,7 @@ export default {
                 offsetY: 120
               },
               value: {
-                show: true,
+                show: false,
                 offsetY: 76,
                 fontSize: "22px",
                 color: undefined,
@@ -77,11 +94,10 @@ export default {
         },
         fill: {
           type: "gradient",
-          colors: ["#08ccfc", "#000"],
+          colors: ["#08ccfc"],
           gradient: {
-            shade: "dark",
+            shade: "light",
             shadeIntensity: 0.15,
-            gradientToColors: ["#91e7fc"], // optional, if not defined - uses the shades of same color in series
             inverseColors: false,
             opacityFrom: 1,
             opacityTo: 1,
@@ -95,9 +111,24 @@ export default {
       }
     };
   },
-
-  props: {
-    done: Number
+  methods: {
+    updateSeries(newSeries, animated) {
+      this.series = newSeries;
+    },
+    formatValue(done, goal) {
+      return (done / goal) * 100;
+    }
+  },
+  watch: {
+    callDone: function() {
+      const leftToGo = this.callGoal - this.callDone;
+      this.title = leftToGo + " left to go";
+      this.updateSeries([this.formatValue(this.callDone, this.callGoal)], true);
+      if (this.callDone == this.callGoal) {
+        this.isDone = true;
+        this.title = "You've reached your goal!";
+      }
+    }
   }
 };
 </script>
--- call-goal/src/views/Dashboard.vue
+++ call-goal/src/views/Dashboard.vue
@@ -1,35 +1,57 @@
 <template>
   <div class="dashboard">
     <img class="logo fadeInDown" alt="Ciara logo" src="../assets/logoCiara.png">
-    <h3 class="title fadeInDown delay-1s">Let's go man</h3>
-    <h2 class="title fadeInDown delay-1s">{{leftToGo}}</h2>
-    <b-button class="addButton fadeInUp" v-on:click="buttonClicked">{{buttonMsg}}</b-button>
-    <Gauge v-bind:done="done" v-bind:goal="goal"/>
+    <!-- <h3 class="title fadeInDown delay-1s">Let's go man</h3> -->
+    <div class="gauge">
+      <b-button :class="[isDone? shakeButton:addButton]" v-on:click="buttonClicked">{{buttonMsg}}</b-button>
+      <Gauge :callDone="done" :callGoal="goal"/>
+    </div>
   </div>
 </template>
 
 <style lang="less">
+.gauge {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  height: 80%;
+}
 .dashboard {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 .addButton {
-  margin: 50px 0px 0px 0px;
-}
-.title {
-  margin: 50px 0px 0px 0px;
+  margin: 20px 0px 10px 0px;
+  animation-name: "fadeInDown";
+  animation-duration: 1.5s;
 }
 
 .logo {
   max-width: 100px;
   max-height: 100px;
+  margin: 10px 0px 10px 0px;
   width: auto;
   height: auto;
   animation-duration: 1.2s;
   animation-delay: 0.2s;
 }
 
+.hide {
+  display: none;
+}
+
+.congratMsg {
+  animation-name: "fadeInDown";
+  animation-delay: 5s;
+}
+
+.shakeButton {
+  margin: 10px 0px 10px 0px;
+  animation-name: "shake";
+  animation-duration: 0.7s;
+}
 .slider {
   margin: 20px 0px 50px 0px;
 }
@@ -37,29 +59,43 @@
 
 <script>
 import Gauge from "@/components/Gauge.vue";
-import Button from "@/components/Button.vue";
 import store from "../store";
 
 export default {
   name: "dashboard",
   components: {
-    Gauge,
-    Button
+    Gauge
+  },
+  updated() {
+    // When reaches goal
+    if (this.done == this.goal) {
+      // Do something with the labels
+      this.buttonMsg = "Made " + this.goal + " calls";
+      this.isDone = true;
+    }
   },
   mounted() {
-    this.goal = this.$store.state.callGoal;
+    // Get goal from store
+    this.goal = store.state.callGoal;
   },
   data() {
     return {
       buttonMsg: "Call +",
-      leftToGo: "89 to go",
       goal: 0,
-      done: 0
+      done: 0,
+      isDone: false,
+      congratChart: "congratChart",
+      congratMsg: "congratMsg",
+      shakeButton: "shakeButton",
+      addButton: "addButton",
+      hide: "hide"
     };
   },
   methods: {
     buttonClicked() {
-      store.dispatch("setCallDone", this.done++);
+      if (this.done < this.goal) {
+        this.done++;
+      }
     }
   }
 };
